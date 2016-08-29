e48a72038c8c178efb859ad401e030a24806b7f1
connor braa <braa@Connors-MacBook-Air.local>
2016-08-28 17:24:00 -0700

    initial commit

---
+++ .bash_profile
@@ -0,0 +1,26 @@
+
+# neovim setup
+alias vim=nvim
+
+# fasd init
+eval "$(fasd --init auto)"
+
+# partial up arrow bash completion
+echo '# arrow up
+"\e[A":history-search-backward
+## arrow down
+"\e[B":history-search-forward' > ~/.inputrc
+
+# shell color
+export CLICOLOR=1
+
+# aliases
+alias gst='git status'
+
+# git autocomplete
+source ~/.dotfiles/.git-completion.bash
+
+# install bash completion
+if [ -f $(brew --prefix)/etc/bash_completion ]; then
+    . $(brew --prefix)/etc/bash_completion
+fi
---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+
+# get directory this script is in
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# link up git dotfiles to home dotfiles
+ln -s $dir/.bash_profile ~/.bash_profile 
+ln -s $dir/.gitconfig ~/.gitconfig 
+
+# install brew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install nvim
+brew install nvim/nvim/nvim
+
+# command line utilities
+brew install git
+brew install fasd
+brew install ag
+brew install jq
+brew install aria2

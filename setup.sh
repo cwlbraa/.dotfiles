9d83ff79b5529910a892a70557dc863f2013dc1a
Connor Braa <connor.braa@gmail.com>
2020-08-23 14:12:36 -0700

    i3 polishing
    - switch lock to always use lightdm instead of i3lock bs
    - add icons to status bar
    - move dotfiles to their own directory
    - make rofi cuter
    - customize i3 gruvbox theme a bit 4 cuter floating windows
    - clean up other crufty nonsense & dead code

--- setup.sh
+++ setup.sh
@@ -8,13 +8,7 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # link up git dotfiles to home dotfiles
 ln -sf $dir/.gitconfig ~/.gitconfig
 ln -sf $dir/zsh/zshrc ~/.zshrc
-ln -sf $dir/i3config ~/.i3/config
-ln -sf $dir/i3status ~/.i3status.conf
+ln -sf $dir/i3/config ~/.i3/config
+ln -sf $dir/i3/.i3status.conf ~/.i3status.conf
 ln -sf $dir/Xresources ~/.Xresources
 ln -sf $dir/.spacemacs ~/.spacemacs
-
-# install rust toolchain
-# curl https://sh.rustup.rs -sSf | sh
-# fast simd ag
-#cargo install ripgrep
-#export PATH="$HOME/.cargo/bin:$PATH"

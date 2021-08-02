00d5fb87cff093cd19551cdd9cfc1b6ed92f4c15
Connor Braa <cbraa@brex.com>
2021-08-02 10:59:00 -0700

    disable snippets & fancy lisp nonsense

--- doom.d/init.el
+++ doom.d/init.el
@@ -18,7 +18,7 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       (company +tng) ; the ultimate code completion backend
+       (company +childframe) ; the ultimate code completion backend
        ; (ivy +prescient +icons)   ; a search engine for love and life
        (vertico +icons)
 
@@ -45,7 +45,7 @@
        vi-tilde-fringe                  ; fringe tildes to mark beyond EOB
        ;; (window-select :numbers)         ; visually switch windows
        workspaces       ; tab emulation, persistence & separate workspaces
-       ;; zen               ; distraction-free coding or writing
+       zen               ; distraction-free coding or writing
 
        :editor
        (evil +everywhere)               ; come to the dark side, we have cookies
@@ -56,9 +56,9 @@
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
-       (parinfer +rust)   ; turn lisp into python, sort of
+       ;;(parinfer +rust)   ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
-       snippets                       ; my elves. They type so I don't have to
+       ;;snippets                       ; my elves. They type so I don't have to
        word-wrap                      ; soft wrapping with language-aware indent
 
        :emacs
--- doom.d/packages.el
+++ doom.d/packages.el
@@ -12,6 +12,7 @@
 (package! leetcode)
 (package! bazel)
 (package! jsonnet-mode)
+(package! protobuf-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:

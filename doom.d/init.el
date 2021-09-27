d31b854e0ba05b90946ffe5de61bc27de7a9dfac
Connor Braa <cbraa@brex.com>
2021-09-26 21:52:39 -0700

    disable childframe (maybe itll be faster on macos)

--- doom.d/init.el
+++ doom.d/init.el
@@ -18,7 +18,7 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       (company +childframe) ; the ultimate code completion backend
+       company ; the ultimate code completion backend
        ; (ivy +prescient +icons)   ; a search engine for love and life
        (vertico +icons)
 
@@ -34,18 +34,18 @@
        (ligatures +extra)       ; ligatures and symbols to make your code pretty again
        minimap           ; show a map of the code on the side
        modeline    ; snazzy, Atom-inspired modeline, plus API
-       ;;nav-flash         ; blink cursor line after big motions
+       nav-flash         ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints                    ; highlight the region an operation acts on
-       (popup +defaults)          ; tame sudden yet inevitable temporary windows
+       (popup +all)          ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
        ;;treemacs          ; a project drawer, like neotree but cooler
-       unicode           ; extended unicode support for various languages
+       ;; unicode           ; extended unicode support for various languages
        vc-gutter                        ; vcs diff in the fringe
        vi-tilde-fringe                  ; fringe tildes to mark beyond EOB
        ;; (window-select :numbers)         ; visually switch windows
        workspaces       ; tab emulation, persistence & separate workspaces
-       zen               ; distraction-free coding or writing
+       ;; zen               ; distraction-free coding or writing
 
        :editor
        (evil +everywhere)               ; come to the dark side, we have cookies
@@ -72,7 +72,7 @@
        vterm                            ; the best terminal emulation in Emacs
 
        :checkers
-       (syntax +childframe)        ; tasing you for every semicolon you forget
+       syntax        ; tasing you for every semicolon you forget
 
        :tools
        ;;ansible
@@ -127,7 +127,7 @@
        (java +meghanada) ; the poster child for carpal tunnel syndrome
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
-       kotlin            ; a better, slicker Java(Script)
+       (kotlin +lsp)       ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
        ;;lean
        ;;factor

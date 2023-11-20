49579911d13f417ffbabef854f55784f1bd2cbc1
Connor Braa <cwlbraa@users.noreply.github.com>
2023-11-20 14:59:12 -0800

    git blame binding

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -3,7 +3,29 @@ return {
   { "folke/noice.nvim", enabled = false },
 
   { "tpope/vim-commentary" },
-  { "tpope/vim-fugitive" },
+  { "FabijanZulj/blame.nvim", keys = { { "<leader>gb", "<cmd>ToggleBlame<cr>", desc = "blame" } } },
   { "tpope/vim-rhubarb" },
   { "tpope/vim-vinegar" },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
 }

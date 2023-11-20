1fbe55b7395b520ebab920fc130c2af443393506
Connor Braa <cwlbraa@users.noreply.github.com>
2023-11-20 15:05:57 -0800

    having figured out iterm color stupidity, go back to light

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.o.background = "dark"
+vim.o.background = "light"
 
 vim.o.autoread = true
 vim.o.autowriteall = true

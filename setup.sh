71c93cf9efe624bba4fe91f869a30b09557c5480
Connor Braa <cwlbraa@users.noreply.github.com>
2024-02-01 15:35:20 -0800

    init antigen for rdes

--- setup.sh
+++ setup.sh
@@ -3,7 +3,11 @@
 set -x -e
 
 # get directory this script is in
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+pushd $dir
+git submodule update --init
+popd
 
 # link up git dotfiles to home dotfiles
 ln -sf $dir/.gitconfig ~/.gitconfig
@@ -13,10 +17,9 @@ ln -sf $dir/nvim ~/.config/
 ln -sf $dir/.ideavimrc ~/.ideavimrc
 
 case "$(uname -s)" in
-    Linux*)
-        ln -sf $dir/i3/config ~/.i3/config
-        ln -sf $dir/i3/.i3status.conf ~/.i3status.conf
-      ;;
-    Darwin*)
-    ;;
+Linux*)
+	ln -sf $dir/i3/config ~/.i3/config
+	ln -sf $dir/i3/.i3status.conf ~/.i3status.conf
+	;;
+Darwin*) ;;
 esac

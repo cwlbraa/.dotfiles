8544fad67e4f6e9acb16682231e2b1ab35fe8fb8
Connor Braa <cwlbraa@users.noreply.github.com>
2024-02-01 16:08:24 -0800

    install eza on linux

--- setup.sh
+++ setup.sh
@@ -24,6 +24,14 @@ Linux*)
 	ln -sf $dir/i3/.i3status.conf ~/.i3status.conf
 	sudo apt update
 	sudo apt install fasd direnv
+	sudo mkdir -p /etc/apt/keyrings
+
+	# install eza from source
+	wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+	echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+	sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+	sudo apt update
+	sudo apt install -y eza
 	;;
 Darwin*) ;;
 esac

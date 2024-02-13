e5b2aade2c1b871e1d41155142904f6cb6882195
Connor Braa <cwlbraa@users.noreply.github.com>
2024-02-13 10:43:10 -0800

    mktemp -d

--- setup.sh
+++ setup.sh
@@ -39,7 +39,7 @@ Linux*)
 	sudo apt update
 	sudo apt install -y eza
 
-	pushd $(mktemp)
+	pushd $(mktemp -d)
 	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 	tar xf lazygit.tar.gz lazygit
